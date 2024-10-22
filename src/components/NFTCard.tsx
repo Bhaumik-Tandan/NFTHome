169e0fdd5ad74427e319da09cc8e9549e9d48981
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2024-10-22 11:58:05 +0530

    [add] nft card

--- src/components/NFTCard.tsx
+++ src/components/NFTCard.tsx
@@ -1,11 +1,83 @@
-import React from 'react'
+import React from 'react';
+import COLOR from '../constants/color';
 
-function NFTCard() {
+const NFTCard = () => {
   return (
-    <div>
-        <img src={require("../assets/Image Placeholder.png")} alt="NFT" />
+    <div style={styles.card}>
+      <div style={styles.imageContainer}>
+        <img 
+          src={require("../assets/Image Placeholder.png")}
+          alt="NFT" 
+          style={styles.mainImage}
+        />
+      </div>
+
+      {/* Card Content */}
+      <div style={styles.label}>
+        {/* Title */}
+        <p style={styles.title}>
+          Space Walking
+        </p>
+
+        {/* Creator Info */}
+        <div style={styles.creatorContainer}>
+          <img 
+            src={require("../assets/Avatar.png")}
+            alt="Avatar"
+            style={styles.avatarImage}
+          />
+          <span style={styles.creatorName}>
+            Animakid
+          </span>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+const styles:any = {
+  card: {
+    maxWidth: '320px',
+    borderRadius: '12px',
+    overflow: 'hidden',
+    backgroundColor: COLOR.BACKGROUND_SECONDARY
+  },
+  imageContainer: {
+    position: 'relative',
+    width: '100%'
+  },
+  mainImage: {
+    width: '100%',
+    height: '256px',
+    objectFit: 'cover'
+  },
+  label: {
+    padding: '16px',
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '12px',
+    backgroundColor: COLOR.BACKGROUND_SECONDARY
+  },
+  title: {
+    fontSize: '20px',
+    fontWeight: '600',
+    color: '#FFFFFF',
+    margin: 0
+  },
+  creatorContainer: {
+    display: 'flex',
+    alignItems: 'center',
+    gap: '12px'
+  },
+  avatarImage: {
+    width: '32px',
+    height: '32px',
+    borderRadius: '50%'
+  },
+  creatorName: {
+    color: '#CCCCCC',
+    fontSize: '14px'
+  }
+};
 
 export default NFTCard;
